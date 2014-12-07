d6422ab8a1d91959e3bfa29fcdbedaf4d3832e87
Harald Pichler <harald@the-develop.net>
2014-12-07 17:55:51 +0100

    change pin onboard led and switch off unused grove moduels

--- www/generators/arduino/base.js
+++ www/generators/arduino/base.js
@@ -223,8 +223,8 @@ Blockly.Arduino.base_map = function() {
 
 Blockly.Arduino.inout_buildin_led = function() {
   var dropdown_stat = this.getTitleValue('STAT');
-  Blockly.Arduino.setups_['setup_output_13'] = 'pinMode(13, OUTPUT);';
-  var code = 'digitalWrite(13,'+dropdown_stat+');\n'
+  Blockly.Arduino.setups_['setup_output_4'] = 'pinMode(4, OUTPUT);';
+  var code = 'digitalWrite(4,'+dropdown_stat+');\n'
   return code;
 };
 
